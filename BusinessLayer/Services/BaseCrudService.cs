0901ad43c4e5dd6ccef3e32888fd6fc8e5bc7775
Roqia3 <136268858+Roqia3@users.noreply.github.com>
2023-06-12 01:13:19 +0300

    Update BaseCrudService.cs

--- BusinessLayer/Services/BaseCrudService.cs
+++ BusinessLayer/Services/BaseCrudService.cs
@@ -7,6 +7,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BusinessLayer.Services.Email;
+
 
 namespace MVC_Example.BusinessLayer.Services
 {
